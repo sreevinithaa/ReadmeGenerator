5c53a0b6dbf43aa24db458a9253ef9468975c507
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-04-17 18:06:01 +0800

    Update license switch case"

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,44 +5,96 @@ function renderLicenseBadge(license) {
     let badge;
     switch (license) {
       case "MIT":
-        badge = "https://img.shields.io/badge/License-MIT-yellow.svg";
+        badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
         break;
       case "Boost 1.0":
-        badge = "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg";
+        badge =
+          "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
         break;
       case "Apache 2.0":
-        badge = "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg";
+        badge =
+          "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
         break;
       case "BSD 2-Clause":
-        badge = "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
+        badge =
+          "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
         break;
       case "BSD 3-Clause":
-        badge = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+        badge =
+          "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
         break;
-      case "BSD 4-Clause":
-        badge = "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg";
+      case "CC0 1.0":
+        badge =
+          "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)";
         break;
 
-      case "Boost 1.0":
-        badge = "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg";
+      case "CC BY 4.0":
+        badge =
+          "[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)";
         break;
-      case "Boost 1.0":
-        badge = "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg";
+      case "CC BY-SA 4.0":
+        badge = "[![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)";
         break;
-      case "Boost 1.0":
-        badge = "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg";
+      case "CC BY-NC 4.0":
+        badge = "[![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc/4.0/)";
         break;
-      case "Boost 1.0":
-        badge = "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg";
+      case "CC BY-ND 4.0":
+        badge = "[![License: CC BY-ND 4.0](https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nd/4.0/)";
         break;
-      case "Boost 1.0":
-        badge = "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg";
+        case "CC BY-NC-SA 4.0":
+            badge = "[![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)";
+            break;
+            case "CC BY-NC-ND 4.0":
+                badge = "[![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-nd/4.0/)";
+                break;
+      case "EPL 1.0":
+        badge = "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
         break;
-      case "Boost 1.0":
-        badge = "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg";
+      case "GPL v3":
+        badge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+        break;
+      case "GPL v2":
+        badge = "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
+        break;
+      case "AGPL v3":
+        badge = "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+        break;
+      case "LGPL v3":
+        badge = "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+        break;
+      case "FDL 1.3":
+        badge = "[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)";
+        break;
+      case "Hippocratic 2.1":
+        badge = "[![License: Hippocratic 2.1](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)](https://firstdonoharm.dev)";
+        break;
+      case "Hippocratic 3.0":
+        badge = "[![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg)](https://firstdonoharm.dev)";
+        break;
+      case "IPL 1.0":
+        badge = "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+        break;
+      case "ISC":
+        badge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+        break;
+      case "MPL 2.0":
+        badge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+        break;
+      case "Open Data Commons Attribution":
+        badge = "[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)](https://opendatacommons.org/licenses/by/)";
+        break;
+      case "ODbL":
+        badge = "[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)";
+        break;
+      case "Artistic-2.0":
+        badge = "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic_2.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
+        break;
+      case "Open Font-1.1":
+        badge = "[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)";
         break;
       default:
         console.log("Score value is neither 10 or 20");
+        return badge;
     }
   } else {
     return "";
